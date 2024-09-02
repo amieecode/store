04d256aa742891647bba28dad5015401ca6cf315
amieecode <enwelumchiamaka@gmail.com>
2024-09-02 22:36:55 +0100

    Create new app cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
+    
 ]
 
 MIDDLEWARE = [
