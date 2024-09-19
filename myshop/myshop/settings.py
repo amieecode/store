0b9b449c04ac5abd2e60241cf09f990e8056b837
amieecode <enwelumchiamaka@gmail.com>
2024-09-19 23:35:23 +0100

    setting

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
